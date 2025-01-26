cb09be664e489279f4ad9acff1576d56e96927ef
G0tem <bindirin@yahoo.com>
2025-01-27 03:06:24 +0500

    fix logic db redis

--- bet-maker/main.py
+++ bet-maker/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import List, Dict
 import uuid
 import time
 import httpx
+from redis.asyncio import Redis
 
 app = FastAPI()
+
+
+redis_client = Redis(host="redis", port=6379, db=0)
 
 
 class Bet(BaseModel):
@@ -15,9 +18,17 @@ class Bet(BaseModel):
     status: str = "pending"
 
 
-bets: List[Bet] = []
-
 LINE_PROVIDER_URL = "http://line-provider:8080"
+
+
+@app.on_event("startup")
+async def startup():
+    await redis_client.ping()  # Проверка подключения к Redis
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await redis_client.close()  # Закрытие соединения с Redis
 
 
 @app.get("/events")
@@ -42,23 +53,33 @@ async def place_bet(event_id: str, amount: float):
             raise HTTPException(status_code=400, detail="Betting deadline has passed")
 
     bet = Bet(id=str(uuid.uuid4()), event_id=event_id, amount=amount)
-    bets.append(bet)
+    await redis_client.hset(
+        f"bet:{bet.id}",
+        mapping={"event_id": bet.event_id, "amount": bet.amount, "status": bet.status},
+    )
     return bet
 
 
 @app.post("/update_bet_status")
 async def update_bet_status(event_id: str, state: str):
-    for bet in bets:
-        if bet.event_id == event_id:
-            if state == "FINISHED_WIN":
-                bet.status = "win"
-
-            elif state == "FINISHED_LOSE":
-                bet.status = "lose"
-
+    async for key in redis_client.scan_iter("bet:*"):
+        bet_data = await redis_client.hgetall(key)
+        if bet_data[b"event_id"].decode() == event_id:
+            await redis_client.hset(key, "status", state.lower())
     return {"message": "Bet status updated"}
 
 
 @app.get("/bets")
 async def get_bets():
+    bets = []
+    async for key in redis_client.scan_iter("bet:*"):
+        bet_data = await redis_client.hgetall(key)
+        bets.append(
+            {
+                "id": key.decode().split(":")[1],
+                "event_id": bet_data[b"event_id"].decode(),
+                "amount": float(bet_data[b"amount"]),
+                "status": bet_data[b"status"].decode(),
+            }
+        )
     return bets
